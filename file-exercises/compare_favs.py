cd9bc1c3a993bcf2b5a75f9da60687a30e7f80c3
Nina <nina@abc.com>
2015-11-16 20:29:12 -0800

    adding the compar favs file simplified with a function

--- file-exercises/compare_favs.py
+++ file-exercises/compare_favs.py
@@ -1,12 +1,31 @@
-liz = open("liz_fav_foods.txt")
-liz_list = liz.readlines()
-print liz_list
+#liz = open("liz_fav_foods.txt")
+#liz_list = liz.readlines()
+#print liz_list
 
 
-liz.close()
+#liz.close()
 
-laura = open ("laura_fav_foods.txt")
-laura_list = laura.readlines()
-print laura_list
+#laura = open ("laura_fav_foods.txt")
+#laura_list = laura.readlines()
+#print laura_list
 
-laura.close()
+#laura.close()
+
+
+#compare_ = open("laura_fav_foods.txt" and "liz_fav_foods.txt")
+
+#compare_list = compare_favs.readlines()
+#print compare_list
+
+#compare_favs.close()
+
+def compare_lists(name):
+	my_file = open(name)
+	name_list = my_file.readlines()
+	print name_list
+	my_file.close()
+
+compare_lists("liz_fav_foods.txt")
+compare_lists("laura_fav_foods.txt")
+
+
