ff615c20d0b1e00709ad742ed5037b55d345bfc2
Nina <nina@abc.com>
2015-11-16 20:04:53 -0800

    adding the comparing favs files for Liz and Laura

---
+++ file-exercises/compare_favs.py
@@ -0,0 +1,12 @@
+liz = open("liz_fav_foods.txt")
+liz_list = liz.readlines()
+print liz_list
+
+
+liz.close()
+
+laura = open ("laura_fav_foods.txt")
+laura_list = laura.readlines()
+print laura_list
+
+laura.close()
